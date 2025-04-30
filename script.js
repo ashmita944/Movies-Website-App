637de0e9a52c1f52d5f12c2f0db5199432fa8080
ASHMITA <ashmita.in.com@gmail.com>
2025-04-29 21:06:11 -0400

    Script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const inputBox = document.querySelector('.inputBox');
 
 const getMovieInfo = async (movie) =>{
     try {
-        const myAPIKey = "2d6a80ce";
+        const myAPIKey = "..........";
         const url =`http://www.omdbapi.com/?apikey=${myAPIKey}&t=${movie}`;
 
         const response = await fetch(url);
